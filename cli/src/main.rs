90b625f904223a321f4781c12f0fccc9b728cfbf
Samueltgg12 <samuelhjf4@gmail.com>
2026-09-10 00:20:40 -0300

    Fixed UART Stuff.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -48,9 +48,8 @@ fn main() -> Result<()> {
     let (uart1_tx, uart1_rx) = mpsc::channel::<u8>();
     let (uart2_tx, uart2_rx) = mpsc::channel::<u8>();
 
-    // Clone senders for the threads
+    // Clone sender for the stdin thread
     let uart1_tx_stdin = uart1_tx.clone();
-    let uart2_rx_stdout = uart2_rx.clone();
 
     // Spawn thread to read from stdin and send to UART1 (console input)
     thread::spawn(move || {
@@ -72,7 +71,7 @@ fn main() -> Result<()> {
     thread::spawn(move || {
         let mut stdout = io::stdout();
         loop {
-            match uart2_rx_stdout.recv() {
+            match uart2_rx.recv() {
                 Ok(byte) => {
                     if stdout.write_all(&[byte]).is_err() {
                         break;
@@ -86,7 +85,7 @@ fn main() -> Result<()> {
         }
     });
 
-    let mut emulator = Emulator::with_ram(args.ram_mb, uart1_tx, uart1_rx, uart2_tx, uart2_rx);
+    let mut emulator = Emulator::with_ram(args.ram_mb, uart1_tx, uart1_rx, uart2_tx);
     emulator.load_prom(&args.prom)?;
 
     o2rust::log::info_msg(&format!(
